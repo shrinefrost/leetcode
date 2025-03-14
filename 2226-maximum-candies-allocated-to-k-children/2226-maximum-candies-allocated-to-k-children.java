1c45c3ba89786a00262599a0bdc98fe448d59706
Suraj Pandey <surajpandey1115@gmail.com>
2025-03-14 18:39:10 +0530

    Time: 30 ms (48.92%), Space: 63 MB (69.89%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maximumCandies(int[] candies, long k) {
+        int left = 1;
+        int right = Arrays.stream(candies).max().getAsInt();
+        int ans = 0;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long count = 0;
+            
+            for (int c : candies) {
+                count += c / mid;
+            }
+            
+            if (count >= k) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+}
