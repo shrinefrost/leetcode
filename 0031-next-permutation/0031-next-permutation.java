3a1ee68242bcdcd0ca69a743640e73d6caf15984
Suraj Pandey <surajpandey1115@gmail.com>
2025-03-16 11:06:29 +0530

    Time: 3 ms (5.53%), Space: 43.2 MB (36.87%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,43 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        //observation 1 to find the first dip
+        int dip =0;
+        int dipIndex=0;
+        int n = nums.length;
+
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                dip++;
+                dipIndex =i;
+                System.out.println(i);
+                break;
+            }
+        }
+        if(dip==0){
+            for (int i = 0, j = nums.length - 1; i < j; i++, j--) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+            return;
+        }
+
+        // observation 2 find the integer which will take place at dip index
+        for(int i = n-1;i>dipIndex;i--){
+            if(nums[i]>nums[dipIndex]){
+                int temp = nums[i];
+                nums[i] = nums[dipIndex];
+                nums[dipIndex]=temp;
+                break;
+            }
+        }
+
+        //observation 3 now arrange the element of the array in smaller to greater
+
+        for(int i=dipIndex+1, j=n-1; i<j; i++,j--){
+            int temp = nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+        }
+    }
+}
