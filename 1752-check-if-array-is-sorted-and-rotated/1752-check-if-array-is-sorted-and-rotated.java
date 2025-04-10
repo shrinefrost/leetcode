a81d59b4ea5bfad6864e21d911a87c7c7ae3f323
Suraj Pandey <surajpandey1115@gmail.com>
2025-04-10 15:47:07 +0530

    Time: 0 ms (100%), Space: 41.8 MB (6.53%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,21 +1,33 @@
 public class Solution {
     public boolean check(int[] nums) {
         int n = nums.length;
-        int count = 0;
+        int violation = 0;
 
-        // Count the number of times nums[i-1] > nums[i]
-        for (int i = 1; i < n; i++) {
-            if (nums[i - 1] > nums[i]) {
-                count++;
+        for ( int i =0 ; i<nums.length; i++) {
+            if(nums[i]>nums[(i+1)%nums.length]){
+                violation++;
+                if(violation>1) return false;
             }
         }
+        return true;
 
-        // Check for the rotation wrap-around: last > first
-        if (nums[n - 1] > nums[0]) {
-            count++;
-        }
 
-        // Return true if there's at most one violation
-        return count <= 1;
+
+
+
+        // // Count the number of times nums[i-1] > nums[i]
+        // for (int i = 1; i < n; i++) {
+        //     if (nums[i - 1] > nums[i]) {
+        //         count++;
+        //     }
+        // }
+
+        // // Check for the rotation wrap-around: last > first
+        // if (nums[n - 1] > nums[0]) {
+        //     count++;
+        // }
+
+        // // Return true if there's at most one violation
+        // return count <= 1;
     }
 }
